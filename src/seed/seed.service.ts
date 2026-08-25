4caed55772b49fa8f56cfa07268ebc110acb260a
OscarB <oscartic@gmail.com>
2026-08-25 17:59:49 -0400

    feat: Introduce SeedModule and implement basic pokémon seeding

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance} from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  async executedSeed() {
+    
+    try {
+      const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+      data.results.forEach(async ({ name, url }) => {
+        const segments = url.split('/');
+        const no: number = +segments[segments.length - 2];
+        console.log('SeedService executedSeed', { name, no });
+      });
+      
+    } catch (error) {
+      console.error('SeedService executedSeed error', { error }); 
+    }
+
+
+    return 'Seeds runned';
+  }
+
+}
